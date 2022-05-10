b6b304859cade79b0571a6c33ef61e376175ecdb
Enriyuu <gm3r23@gmail.com>
2022-05-09 23:49:05 -0300

    .

--- P2/Lab-de-Programação/aulas-de-C/Aula05.c
+++ P2/Lab-de-Programação/aulas-de-C/Aula05.c
@@ -7,10 +7,15 @@ int main ()
     setlocale(LC_ALL, "Portuguese_Brazil");
 
     int MinhaIdade;
-
     MinhaIdade = 19;
 
-    printf("Sua idade é %d anos.\n", MinhaIdade);
+    int MaeIdade;
+    MaeIdade = 48;
+                            //formas de declarar variável
+    int PaiIdade = 49;
+
+    printf("Minha idade é %d anos\nA idade da minha mãe é %d anos\nA idade do meu pai é %d anos\n",
+    MinhaIdade, PaiIdade, MaeIdade);
 
     system("pause");
     return 0;
