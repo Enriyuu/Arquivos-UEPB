c7a03ea81449d97dc3453f64d592fa5b4c737b92
Enriyuu <gm3r23@gmail.com>
2022-05-10 00:46:23 -0300

    .

--- P2/Lab-de-Programação/aulas-de-C/Aula08.c
+++ P2/Lab-de-Programação/aulas-de-C/Aula08.c
@@ -6,6 +6,13 @@ int main ()
 {
     setlocale(LC_ALL, "Portuguese_Brazil");
 
+    int inteira = 10;
+    float decimal = 10.5;
+    char caractere = '?a';
+
+    printf("%d\n", inteira);
+    printf("%f\n", decimal);
+    printf("%c\n", caractere);
 
 
     system("pause");
